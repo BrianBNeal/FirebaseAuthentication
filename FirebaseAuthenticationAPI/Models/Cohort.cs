82e9b132d787be6920948756f1576cd0598e49bc
Brian Neal <brianbneal@gmail.com>
2022-05-07 19:37:30 -0500

    initial commit

--- FirebaseAuthenticationAPI/Models/Cohort.cs
+++ FirebaseAuthenticationAPI/Models/Cohort.cs
@@ -2,9 +2,9 @@
 {
     public class Cohort
     {
-        public int Id { get; set; }
+        public string Id { get; set; }
         public CohortType Type { get; set; }
-        public int Number { get; set; }
+        public string LongName { get; set; }
         public List<Student> Students { get; set; }
         public List<Instructor> Instructors { get; set; }
     }
@@ -13,8 +13,5 @@
     {
         DaytimeWebDev = 0,
         EveningWebDev = 1,
-        DaytimeUIUX = 2,
-        DaytimeDataAnalytics = 3,
-        DaytimeDataScience = 4,
     }
 }
--- FirebaseAuthenticationAPI/Models/Group.cs
+++ FirebaseAuthenticationAPI/Models/Group.cs
@@ -5,7 +5,8 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public int CohortId { get; set; }
+        public Cohort Cohort { get; set; }
         public List<Student> Members { get; set; }
-        public Instructor? Mentor { get; set; }
+        public Instructor Mentor { get; set; }
     }
 }
